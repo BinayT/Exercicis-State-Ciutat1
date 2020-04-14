61c8ab106f5105f2877d722963060e63ecdb54a4
Binay <binayprogramming@gmail.com>
2020-04-14 15:40:39 +0200

    State Ciutats 1

--- src/components/ListaCiudades/CiudadesFunctions.jsx
+++ src/components/ListaCiudades/CiudadesFunctions.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { CIUTATS_CAT_20K } from "../../datos";
+import { CIUTATS_CAT_20K, CIUTATS } from "../../datos";
+
+let ciudades = CIUTATS_CAT_20K.map((el) => {
+  el.poblacio = el.poblacio + "";
+  el.poblacio = el.poblacio.replace(/\./g, "") * 1;
+  return el;
+});
 
 const ordenarCiudadesAlfabeticamente = (x, y) => {
   if (x.municipi > y.municipi) {
@@ -11,37 +17,37 @@ const ordenarCiudadesAlfabeticamente = (x, y) => {
   return 0;
 };
 
-export const filas1rPaso = CIUTATS_CAT_20K.sort(
-  ordenarCiudadesAlfabeticamente
-).map((el, index) => (
-  <tr key={index}>
-    <td>{el.municipi}</td>
-    <td>{el.provincia}</td>
-    <td>{el.poblacio}</td>
-  </tr>
-));
+export const filas1rPaso = ciudades
+  .sort(ordenarCiudadesAlfabeticamente)
+  .map((el, index) => (
+    <tr key={index}>
+      <td>{el.municipi}</td>
+      <td>{el.provincia}</td>
+      <td>{el.poblacio}</td>
+    </tr>
+  ));
 
 const ordenarCiudadesPorPoblacion = (a, b) => {
   return b.poblacio - a.poblacio;
 };
 
-export const filas2oPaso = CIUTATS_CAT_20K.sort(
-  ordenarCiudadesPorPoblacion
-).map((el, index) => (
-  <tr key={index}>
-    <td>{el.municipi}</td>
-    <td>{el.provincia}</td>
-    <td>{el.poblacio}</td>
-  </tr>
-));
+export const filas2oPaso = ciudades
+  .sort(ordenarCiudadesPorPoblacion)
+  .map((el, index) => (
+    <tr key={index}>
+      <td>{el.municipi}</td>
+      <td>{el.provincia}</td>
+      <td>{el.poblacio}</td>
+    </tr>
+  ));
 
-export const filas3rPaso = CIUTATS_CAT_20K.filter(
-  (el) => el.poblacio >= 100000
-).map((el, index) => (
-  <tr key={index}>
-    <td>{el.municipi}</td>
-    <td>{el.provincia}</td>
-    <td>{el.poblacio}</td>
-    <td>{el.comarca}</td>
-  </tr>
-));
+export const filas3rPaso = ciudades
+  .filter((el) => el.poblacio >= 100000)
+  .map((el, index) => (
+    <tr key={index}>
+      <td>{el.municipi}</td>
+      <td>{el.provincia}</td>
+      <td>{el.poblacio}</td>
+      <td>{el.comarca}</td>
+    </tr>
+  ));
